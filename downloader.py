a67c1783870a6a6cf5a512144c3aa2d1cdadfe1c
premod <premodsuraweera1@gmail.com>
2023-10-04 08:54:49 +0530

    fix comment

--- downloader.py
+++ downloader.py
@@ -16,13 +16,13 @@ strm = int(input("Please Provide the index of the stream you want to download: "
 
 video_stream = videos[strm]
 
-# Get the video size for the progress bar
+
 video_size = video_stream.filesize
 
-# Determine the Downloads folder path in Windows
+
 downloads_folder = os.path.join(os.path.expanduser("~"), "Downloads")
 
-# Download the video with a loading bar
+
 with tqdm(total=video_size, unit='B', unit_scale=True, unit_divisor=1024) as pbar:
     video_stream.download(output_path=downloads_folder, filename=yt.title)
     pbar.update(video_size)
